5b0cb829a7ad999ef5d8d72ac69ba4a2202a9255
nelson.zhang <nelson.zhang@mthreads.com>
2023-08-07 14:55:11 +0800

    [MTSS-FG] Support cache previous frame depth,mvec,color.

--- source/plugins/sl.mtss_g/mtss_gEntry.cpp
+++ source/plugins/sl.mtss_g/mtss_gEntry.cpp
@@ -2,6 +2,7 @@
 
 #include <dxgi1_6.h>
 #include <future>
+#include <assert.h>
 
 #include "include/sl.h"
 #include "include/sl_consts.h"
@@ -13,6 +14,7 @@
 #include "source/platforms/sl.chi/compute.h"
 #include "source/plugins/sl.template/versions.h"
 #include "source/plugins/sl.common/commonInterface.h"
+#include "source/plugins/sl.imgui/imgui.h"
 #include "external/nvapi/nvapi.h"
 #include "external/json/include/nlohmann/json.hpp"
 #include "_artifacts/gitVersion.h"
@@ -21,16 +23,21 @@
 
 using json = nlohmann::json;
 
-//! IMPORTANT: This is our include with our constants and settings (if any)
-//!
-#include "include/sl_template.h"
-
 namespace sl
 {
 
 namespace tmpl
 {
 
+
+struct UIStats
+{
+	std::mutex mtx{};
+	std::string mode{};
+	std::string viewport{};
+	std::string runtime{};
+	std::string vram{};
+};
 
 struct MTSSGContext
 {
@@ -42,23 +49,25 @@ struct MTSSGContext
     // Called when plugin is unloaded, destroy any objects on heap here
     void onDestroyContext() {};
 
-    // For example, we can use this template to store incoming constants
-    // 
-    common::ViewportIdFrameData<> constants = { "template" };
+    UIStats uiStats{};
 
     Constants* commonConsts{};
-
     // Our tagged inputs
     CommonResource mvec{};
     CommonResource depth{};
     CommonResource hudLessColor{};
+    sl::chi::Resource prevMvec{};
+    sl::chi::Resource prevDepth{};
+    sl::chi::Resource prevHudLessColor{};
 
     // Compute API
     RenderAPI platform = RenderAPI::eD3D11;
     chi::ICompute* pCompute{};
-
     chi::ICommandListContext* pCmdList{};
     chi::CommandQueue cmdCopyQueue{};
+
+    uint32_t swapChainWidth{};
+    uint32_t swapChainHeight{};
 
     sl::chi::Resource appSurface{};
     sl::chi::Resource generateFrame{};
@@ -121,38 +130,12 @@ Result slSetConstants(const void* data, uint32_t frameIndex, uint32_t id)
 {
     auto& ctx = (*tmpl::getContext());
 
-    // For example, we can set out constants like this
-    // 
-    auto consts = (const TemplateConstants*)data;
-    ctx.constants.set(frameIndex, id, consts);
-    if (consts->mode == TemplateMode::eOff)
-    {
-        // User disabled our feature
-        auto lambda = [/*capture references and data you need*/](void)->void
-        {
-            // Cleanup logic goes here
-        };
-        // Schedule delayed destroy (few frames later)
-        CHI_VALIDATE(ctx.pCompute->destroy(lambda));
-    }
-    else
-    {
-        // User enabled our feature, nothing to do here
-        // but rather in 'templateBeginEvaluation' when
-        // we have access to the command buffer.
-    }
     return Result::eOk;
 }
 
 //! Get settings for our plugin (optional and depending on if we need to provide any settings back to the host)
 Result slGetSettings(const void* cdata, void* sdata)
 {
-    // For example, we can set out constants like this
-    // 
-    // Note that TemplateConstants should be defined in sl_constants.h and provided by the host
-    // 
-    auto consts = (const TemplateConstants*)cdata;
-    auto settings = (TemplateSettings*)sdata;
     return Result::eOk;
 }
 
@@ -170,8 +153,6 @@ Result slFreeResources(sl::Feature feature, const sl::ViewportHandle& viewport)
 
 bool slOnPluginStartup(const char* jsonConfig, void* device)
 {
-    //! Common startup and setup
-    //!     
     SL_PLUGIN_COMMON_STARTUP();
 
     auto& ctx = (*tmpl::getContext());
@@ -212,20 +193,49 @@ bool slOnPluginStartup(const char* jsonConfig, void* device)
     ctx.pCompute->createCommandListContext(ctx.cmdCopyQueue, 1, ctx.pCmdList, "mtss-g ctx");
     assert(ctx.pCmdList != nullptr);
 
+    // ImGUI Plugin not support in out card, it require DX12
+    imgui::ImGUI* ui{};
+    param::getPointerParam(parameters, param::imgui::kInterface, &ui);
+    if (ui)
+    {
+        // Runs async from the present thread where UI is rendered just before frame is presented
+        auto renderUI = [&ctx](imgui::ImGUI* ui, bool finalFrame)->void
+        {
+            imgui::Float4 greenColor{ 0,1,0,1 };
+            imgui::Float4 highlightColor{ 153.0f / 255.0f, 217.0f / 255.0f, 234.0f / 255.0f,1 };
+
+            auto v = api::getContext()->pluginVersion;
+            std::scoped_lock lock(ctx.uiStats.mtx);
+            uint32_t lastFrame, frame;
+            if (api::getContext()->parameters->get(sl::param::dlss::kCurrentFrame, &lastFrame))
+            {
+                ctx.pCompute->getFinishedFrameIndex(frame);
+                if (lastFrame < frame)
+                {
+                    ctx.uiStats.mode = "Mode: Off";
+                    ctx.uiStats.viewport = ctx.uiStats.runtime = {};
+                }
+                if (ui->collapsingHeader(extra::format("sl.mtss-g v{}", (v.toStr() + "." + GIT_LAST_COMMIT_SHORT)).c_str(), imgui::kTreeNodeFlagDefaultOpen))
+                {
+                    ui->text(ctx.uiStats.mode.c_str());
+                }
+            }
+        };
+        ui->registerRenderCallbacks(renderUI, nullptr);
+    }
+
     return true;
 }
 
-//! Main exit point - shutting down our plugin
-//! 
-//! IMPORTANT: Plugins are shutdown in the inverse order based to their priority.
-//! sl.common always shutsdown LAST since it has priority 0
-//!
 void slOnPluginShutdown()
 {
     auto& ctx = (*tmpl::getContext());
 
     ctx.pCompute->destroyResource(ctx.generateFrame);
     ctx.pCompute->destroyResource(ctx.referFrame);
+    ctx.pCompute->destroyResource(ctx.prevDepth);
+    ctx.pCompute->destroyResource(ctx.prevHudLessColor);
+    ctx.pCompute->destroyResource(ctx.prevMvec);
     ctx.pCompute->destroyResource(ctx.appSurface);
 
     ctx.pCompute->destroyCommandListContext(ctx.pCmdList);
@@ -241,7 +251,9 @@ HRESULT slHookCreateSwapChain(IDXGIFactory* pFactory, IUnknown* pDevice, DXGI_SW
     HRESULT hr = S_OK;
 
     auto& ctx = (*tmpl::getContext());
-    
+    ctx.swapChainWidth  = pDesc->BufferDesc.Width;
+    ctx.swapChainHeight = pDesc->BufferDesc.Height;
+
     pDesc->BufferCount = 2;
 
 	chi::ResourceDescription desc;
@@ -251,7 +263,9 @@ HRESULT slHookCreateSwapChain(IDXGIFactory* pFactory, IUnknown* pDevice, DXGI_SW
 
 	ctx.pCompute->createTexture2D(desc, ctx.generateFrame, "generate frame");
 	ctx.pCompute->createTexture2D(desc, ctx.referFrame, "refer frame");
-    
+    ctx.pCompute->createTexture2D(desc, ctx.prevDepth, "previous depth");
+    ctx.pCompute->createTexture2D(desc, ctx.prevHudLessColor, "previous hudless color");
+    ctx.pCompute->createTexture2D(desc, ctx.prevMvec, "previous mvec");
     return hr;
 }
 
@@ -284,27 +298,31 @@ HRESULT slHookPresent(IDXGISwapChain* swapChain, UINT SyncInterval, UINT Flags, 
             ctx.pCompute->getSwapChainBuffer(swapChain, 0, ctx.appSurface);
         }
 
+        sl::Result ret = getTaggedResource(kBufferTypeHUDLessColor, ctx.hudLessColor, 0);
+        assert(ret == sl::Result::eOk);
+        ret = getTaggedResource(kBufferTypeDepth, ctx.depth, 0);
+        assert(ret == sl::Result::eOk);
+        ret = getTaggedResource(kBufferTypeMotionVectors, ctx.mvec, 0);
+        assert(ret == sl::Result::eOk);
+
         // First frame skip generate, just copy and present
         if (ctx.frameId == 1)
         {
-            ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.referFrame, ctx.appSurface);
+            auto status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.referFrame, ctx.appSurface);
+            assert(status == sl::chi::ComputeStatus::eOk);
+
             swapChain->Present(SyncInterval, Flags);
             ctx.state.numFramesActuallyPresented++;
         }
         else
         {
             // Not first frame, use current surface and refer frame to generate frame
-            sl::Result ret = getTaggedResource(kBufferTypeHUDLessColor, ctx.hudLessColor, 0);
-            assert(ret == sl::Result::eOk);
-            ret = getTaggedResource(kBufferTypeDepth, ctx.depth, 0);
-            assert(ret == sl::Result::eOk);
-            ret = getTaggedResource(kBufferTypeMotionVectors, ctx.mvec, 0);
-            assert(ret == sl::Result::eOk);
-
             common::EventData eventData;
             eventData.id = 0;
             eventData.frame = ctx.frameId;
-            common::getConsts(eventData, &ctx.commonConsts);
+            auto getDataResult = common::getConsts(eventData, &ctx.commonConsts);
+            assert(getDataResult == common::GetDataResult::eFound);
+
 
             SL_LOG_INFO("Frame %llu jitterOffset: %f, %f", ctx.frameId, ctx.commonConsts->jitterOffset.x, ctx.commonConsts->jitterOffset.y);
 
@@ -322,35 +340,45 @@ HRESULT slHookPresent(IDXGISwapChain* swapChain, UINT SyncInterval, UINT Flags, 
             // Aim to simulate frame generate algorithm.
             D3D11_BOX box;
             box.left = 0;
-            box.right = 1920 / 2;
+            box.right = ctx.swapChainWidth / 2;
             box.top = 0;
-            box.bottom = 1080;
+            box.bottom = ctx.swapChainHeight;
             box.front = 0;
             box.back = 1;
             reinterpret_cast<ID3D11DeviceContext*>(ctx.pCmdList->getCmdList())->CopySubresourceRegion(
                 static_cast<ID3D11Resource*>(ctx.generateFrame->native), 0, 0, 0, 0,
                 static_cast<ID3D11Resource*>(ctx.referFrame->native), 0, &box);
 
-            box.left = 1920 / 2;
-            box.right = 1920;
+            box.left = ctx.swapChainWidth / 2;
+            box.right = ctx.swapChainWidth;
             reinterpret_cast<ID3D11DeviceContext*>(ctx.pCmdList->getCmdList())->CopySubresourceRegion(
-                static_cast<ID3D11Resource*>(ctx.generateFrame->native), 0, 1920 / 2, 0, 0,
+                static_cast<ID3D11Resource*>(ctx.generateFrame->native), 0, ctx.swapChainWidth / 2, 0, 0,
                 static_cast<ID3D11Resource*>(ctx.appSurface->native), 0, &box);
 
             // Copy current surface to refer frame
-            ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.referFrame, ctx.appSurface);
+            auto status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.referFrame, ctx.appSurface);
+            assert(status == sl::chi::ComputeStatus::eOk);
 
             // Copy generate frame to surface present
-            ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.appSurface, ctx.generateFrame);
+            status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.appSurface, ctx.generateFrame);
+            assert(status == sl::chi::ComputeStatus::eOk);
             swapChain->Present(SyncInterval, Flags);
             ctx.state.numFramesActuallyPresented++;
             ctx.state.status = MTSSGStatus::eOk;
 
             // Copy refer frame to surface present
-            ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.appSurface, ctx.referFrame);
+            status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.appSurface, ctx.referFrame);
+            assert(status == sl::chi::ComputeStatus::eOk);
             swapChain->Present(SyncInterval, Flags);
             ctx.state.numFramesActuallyPresented++;
         }
+
+        auto status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.prevDepth, ctx.depth);
+        assert(status == sl::chi::ComputeStatus::eOk);
+        status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.prevMvec, ctx.mvec);
+        assert(status == sl::chi::ComputeStatus::eOk);
+        status = ctx.pCompute->copyResource(ctx.pCmdList->getCmdList(), ctx.prevHudLessColor, ctx.hudLessColor);
+        assert(status == sl::chi::ComputeStatus::eOk);
     }
 
     ctx.frameId++;
@@ -405,6 +433,8 @@ sl::Result slMTSSGSetOptions(const sl::ViewportHandle& viewport, const sl::MTSSG
     SL_LOG_INFO("MTSS-G Option numFramesToGenerate:%d ", ctx.options.numFramesToGenerate);
     SL_LOG_INFO("MTSS-G Option Mvec Depth Witdh:   %d ", ctx.options.mvecDepthWidth);
     SL_LOG_INFO("MTSS-G Option Mvec Depth Height:  %d ", ctx.options.mvecDepthHeight);
+
+    ctx.uiStats.mode = ctx.options.mode == sl::MTSSGMode::eOn ? "MTSS-G On" : "MTSS-G Off";
 
     return sl::Result::eOk;
 }
